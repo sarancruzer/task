359db75a1631383bd3ab634251aa02499616229e
Saravanan Nandhan <sarancruzer@gmail.com>
2018-04-12 20:23:54 +0530

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
+import { FullLayoutComponent } from './containers/full-layout/full-layout.component';
+import { LandingComponent } from './component/landing/landing.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './component/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '**', redirectTo: '/landing' },
+  {path: '', component: FullLayoutComponent, data: {title: 'Home'},
+  children: [
+    {path: 'landing', component: LandingComponent, data: {title: 'landing'} }
+
+    ]
+  },
+
+];
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { FullLayoutComponent } from './containers/full-layout/full-layout.component';
+import { SimpleLayoutComponent } from './containers/simple-layout/simple-layout.component';
+import { LandingComponent } from './component/landing/landing.component';
+import { SidebarComponent } from './component/sidebar/sidebar.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { LoginComponent } from './component/login/login.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FullLayoutComponent,
+    SimpleLayoutComponent,
+    LandingComponent,
+    SidebarComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
